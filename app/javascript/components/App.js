bb5afb7e00f9fb005b996dd3c629732c65094f28
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-15 09:02:42 -0500

    Set Greeting component up as a route in your App component.

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Greeting from "./Greeting";
 
 class App extends React.Component {
   render () {
     return (
       <BrowserRouter>
       <Routes>
-        <Route exact path="/" render={() => ('Hello gaby!')} />
+        <Route exact path="/" element={<Greeting greeting="Hello Gaby 1!"/>} />
       </Routes>
       </BrowserRouter>
     );
