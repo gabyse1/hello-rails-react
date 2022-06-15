82fd39ee19d26d58c7c9cfc71bbd02ca2f7a2692
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-15 08:53:36 -0500

    Edit routes.rb to have Rails send all requests that are not for the API to our App component.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     get 'greetings', to: 'greetings#index'
   end
+  # Forward all requests to StaticController#index but requests
+  # must be non-Ajax (!req.xhr?) and HTML Mime type (req.format.html?).
+  # This does not include the root ('/') path.
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
   # Defines the root path route ("/")
   root "static#index"
 end
